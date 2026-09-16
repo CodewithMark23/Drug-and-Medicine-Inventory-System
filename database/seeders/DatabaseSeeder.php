e112f271cd99fc9157841dc97a0311dfe50e958d
Mark Villanueva <markchristianvillanueva23@gmail.com>
2026-09-16 08:55:41 +0800

    Update: created a sample database for the medicines

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Medicine;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Default required pharmacist account
+        User::create([
+            'name' => 'pharmacist',
+            'email' => 'pharmacist@pharmacy.local',
+            'password' => Hash::make('med123'),
+        ]);
+
+        // Sample initial medicine records
+        Medicine::create([
+            'name' => 'Biogesic',
+            'category' => 'Analgesic / Antipyretic',
+            'quantity' => 150,
+            'price' => 10.0,
+        ]);
+
+        Medicine::create([
+            'name' => 'Amoxicillin',
+            'category' => 'Antibiotic',
+            'quantity' => 80,
+            'price' => 25.0,
+        ]);
+
+        Medicine::create([
+            'name' => 'Ascorbic Acid',
+            'category' => 'Vitamin C',
+            'quantity' => 500,
+            'price' => 15.0,
+        ]);
+    }
+}
